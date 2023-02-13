fe174d3f8b5bbe2c8d5c70daae548486e5d6ba9c
Tyler Coulson <tecoulson92@gmail.com>
2023-02-12 22:59:01 -0500

    added html route to get single serving information

--- app/api/api_htmx/serving_size.py
+++ app/api/api_htmx/serving_size.py
@@ -12,6 +12,7 @@ from app.api.api_V1 import serving_size as api_servings
 router = APIRouter()
 templates = Jinja2Templates("app/templates")
 
+tabs = {"food", 'active'}
 @router.post(
     "",
     response_class=HTMLResponse,
@@ -24,11 +25,29 @@ def post_servings(*, request: Request, hx_request: str | None = Header(default=N
     context = {
             "request": request,
             "hx_request": hx_request,
-            "servings": [servings_out]
+            "trigger": "click",
+            "servings": [servings_out],
+            "tabs": tabs
         }
 
     return templates.TemplateResponse("servings.html", context)
 
+@router.get(
+    "/{serving_size_id}",
+    response_class=HTMLResponse,
+    status_code=status.HTTP_200_OK,
+)
+def get_serving_size_id(*, request: Request, hx_request: str | None = Header(default=None), serving_size_id: int, db: Session = Depends(deps.get_db)) -> list[schemas.FoodLog]:
+
+    servings_out = jsonable_encoder(api_servings.get_serving_size_id(serving_size_id=serving_size_id, db=db))
+    context = {
+            "request": request,
+            "hx_request": hx_request,
+            "trigger": None,
+            "servings": [servings_out],
+            "tabs": tabs
+        }
+    return templates.TemplateResponse("servings.html", context)
 
 @router.get(
     "/food_id/{food_id}",
@@ -40,6 +59,8 @@ def get_serving_size_by_food(*, request: Request, hx_request: str | None = Heade
     context = {
             "request": request,
             "hx_request": hx_request,
-            "servings": servings_out['servings']
+            "trigger": "click",
+            "servings": servings_out['servings'],
+            "tabs": tabs
         }
     return templates.TemplateResponse("servings.html", context)
