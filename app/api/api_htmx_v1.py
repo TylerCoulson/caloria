d5d44557f7fca8cd9473f13b0b8cf229a0b9f097
Tyler Coulson <tecoulson92@gmail.com>
2023-02-25 01:19:56 -0500

    added tags for htmx route documentation

--- app/api/api_htmx_v1.py
+++ app/api/api_htmx_v1.py
@@ -4,10 +4,10 @@ from app.api.api_htmx import daily_overview, page_index, food, serving_size, use
 
 htmx_router = APIRouter()
 
-htmx_router.include_router(page_index.router)
-htmx_router.include_router(food.router, prefix="/food")
-htmx_router.include_router(user.router, prefix="/user")
-htmx_router.include_router(serving_size.router, prefix="/servings")
-htmx_router.include_router(daily_overview.router, prefix="/daily")
-htmx_router.include_router(food_log.router, prefix="/food_log")
-htmx_router.include_router(get_posting_html.router, prefix="/create")
+htmx_router.include_router(page_index.router, tags=['htmx-index'])
+htmx_router.include_router(food.router, prefix="/food", tags=['htmx-food'])
+htmx_router.include_router(user.router, prefix="/user", tags=['htmx-user'])
+htmx_router.include_router(serving_size.router, prefix="/servings", tags=['htmx-servings'])
+htmx_router.include_router(daily_overview.router, prefix="/daily", tags=['htmx-daily'])
+htmx_router.include_router(food_log.router, prefix="/food_log", tags=['htmx-food_log'])
+htmx_router.include_router(get_posting_html.router, prefix="/create", tags=['htmx-create'])
