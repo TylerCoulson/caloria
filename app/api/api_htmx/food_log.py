6bb8ecd800f211fdac80555763b38971565c9f88
Tyler Coulson <tecoulson92@gmail.com>
2023-03-20 01:49:14 -0400

    updated food_log routes

--- app/api/api_htmx/food_log.py
+++ app/api/api_htmx/food_log.py
@@ -9,9 +9,26 @@ from app import models
 from datetime import date
 from app.api.api_V1 import food_log as api_food_log
 from app import crud
+from app.auth.router import Annotated_Profile
 
-router = APIRouter()
+router = APIRouter(prefix="/food_log")
 templates = Jinja2Templates("app/templates")
+
+@router.get(
+    "/create",
+    response_class=HTMLResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def get_create_log(*, request: Request, hx_request: str | None = Header(default=None), db: Session = Depends(deps.get_db)):
+    
+    context = {
+            "request": request,
+            "hx_request": hx_request,
+            "trigger": 'click'
+        }
+
+    return templates.TemplateResponse("create_food_log.html", context)
+
 
 
 @router.post(
@@ -19,8 +36,8 @@ templates = Jinja2Templates("app/templates")
     response_class=HTMLResponse,
     status_code=status.HTTP_201_CREATED,
 )
-def post_food_log(*, request: Request, hx_request: str | None = Header(default=None), food_log: schemas.FoodLogCreate, db: Session = Depends(deps.get_db)):
-    log = jsonable_encoder(api_food_log.post_food_log(food_log=food_log, db=db))
+async def post_food_log(*, request: Request, hx_request: str | None = Header(default=None), profile: Annotated_Profile, food_log: schemas.FoodLogCreate, db: Session = Depends(deps.get_db)):
+    log = await api_food_log.post_food_log(profile=profile, food_log=food_log, db=db)
     
     context = {
             "request": request,
@@ -34,10 +51,10 @@ def post_food_log(*, request: Request, hx_request: str | None = Header(default=N
     response_class=HTMLResponse,
     status_code=status.HTTP_200_OK,
 )
-def get_food_logs(*, request: Request, hx_request: str | None = Header(default=None), profile_id:int, db: Session = Depends(deps.get_db)):
+async def get_food_logs(*, request: Request, hx_request: str | None = Header(default=None), profile:Annotated_Profile, db: Session = Depends(deps.get_db)):
     
     try:
-        logs = jsonable_encoder(api_food_log.get_food_logs(profile_id=profile_id, db=db))
+        logs = await api_food_log.get_food_logs(profile=profile, db=db)
         context = {
                 "request": request,
                 "hx_request": hx_request,
@@ -54,16 +71,33 @@ def get_food_logs(*, request: Request, hx_request: str | None = Header(default=N
             "message": f"No logs"
         }
         return templates.TemplateResponse("404.html", context)
-        
+                
 @router.get(
-    "/dates",
+    "/{food_log_id:int}",
     response_class=HTMLResponse,
     status_code=status.HTTP_200_OK,
 )
-def get_food_logs_by_profile_date(*, request: Request, hx_request: str | None = Header(default=None), date: date, profile_id:int, db: Session = Depends(deps.get_db)):
+async def get_food_log_id(*, request: Request, hx_request: str | None = Header(default=None), profile: Annotated_Profile, food_log_id: int, db: Session = Depends(deps.get_db)):
+    log = await api_food_log.get_food_log_id(profile=profile, food_log_id=food_log_id, db=db)
+    
+    context = {
+            "request": request,
+            "hx_request": hx_request,
+            "logs": [log],
+            "trigger": None
+        }
+    return templates.TemplateResponse("food_log.html", context)
+
+@router.get(
+    "/{date}",
+    response_class=HTMLResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def get_food_logs_by_profile_date(*, request: Request, hx_request: str | None = Header(default=None), date: date, profile:Annotated_Profile, db: Session = Depends(deps.get_db)):
 
     try:
-        logs = jsonable_encoder(api_food_log.get_food_log_date(date=date, profile_id=profile_id, db=db))['log']
+        logs = await api_food_log.get_food_log_date(date=date, profile=profile, db=db)
+        logs = logs['log']
         context = {
                 "request": request,
                 "hx_request": hx_request,
@@ -77,27 +111,9 @@ def get_food_logs_by_profile_date(*, request: Request, hx_request: str | None = 
         context = {
             "request": request,
             "hx_request": hx_request,
-            "message": f"No logs for {date} by {profile_id}"
+            "message": f"No logs for {date} by {profile}"
         }
         return templates.TemplateResponse("404.html", context)
-        
-@router.get(
-    "/{food_log_id}",
-    response_class=HTMLResponse,
-    status_code=status.HTTP_200_OK,
-)
-def get_food_log_id(*, request: Request, hx_request: str | None = Header(default=None), food_log_id: int, db: Session = Depends(deps.get_db)):
-    log = jsonable_encoder(api_food_log.get_food_log_id(food_log_id=food_log_id, db=db))
-    
-    context = {
-            "request": request,
-            "hx_request": hx_request,
-            "logs": [log],
-            "trigger": None
-        }
-    return templates.TemplateResponse("food_log.html", context)
-
-
 
     
 
@@ -107,7 +123,7 @@ def get_food_log_id(*, request: Request, hx_request: str | None = Header(default
 #     response_class=HTMLResponse,
 #     status_code=status.HTTP_200_OK,
 # )
-# def update_food_log(
+# async def update_food_log(
 #     *, request: Request, hx_request: str | None = Header(default=None), food_log_id: int, food_log_in: schemas.FoodLogBase, db: Session = Depends(deps.get_db)
 # ):
 #     data = get_food_log(food_log_id=food_log_id, db=db)
@@ -120,7 +136,7 @@ def get_food_log_id(*, request: Request, hx_request: str | None = Header(default
 #     "/{food_log_id}",
 #     status_code=status.HTTP_200_OK,
 # )
-# def delete_food_log(*, request: Request, hx_request: str | None = Header(default=None), food_log_id: int, db: Session = Depends(deps.get_db)):
+# async def delete_food_log(*, request: Request, hx_request: str | None = Header(default=None), food_log_id: int, db: Session = Depends(deps.get_db)):
 #     data = get_food_log(food_log_id=food_log_id, db=db)
 
 #     data = crud.delete(_id=food_log_id, db=db, db_obj=data)
--- app/api/api_htmx_v1.py
+++ app/api/api_htmx_v1.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.api_htmx import daily_overview, page_index, food, serving_size, profile, food_log, get_posting_html, predictions
+from app.api.api_htmx import daily_overview, page_index, food, serving_size, profile, food_log, predictions
 from app.auth.html_routers import validation, page_routers
 
 htmx_router = APIRouter(include_in_schema=False)
@@ -10,8 +10,7 @@ htmx_router.include_router(food.router, tags=['htmx-food'])
 htmx_router.include_router(profile.router, tags=['htmx-profile'])
 # htmx_router.include_router(serving_size.router, tags=['htmx-servings'])
 htmx_router.include_router(daily_overview.router, tags=['htmx-daily'])
-htmx_router.include_router(food_log.router, prefix="/food_log", tags=['htmx-food_log'])
-htmx_router.include_router(get_posting_html.router, tags=['htmx-create'])
+htmx_router.include_router(food_log.router, tags=['htmx-food_log'])
 htmx_router.include_router(predictions.router, tags=['htmx-prediction'])
 htmx_router.include_router(page_routers.router, tags=['htmx-auth-pages'])
 htmx_router.include_router(validation.router, tags=['htmx-validation'])
