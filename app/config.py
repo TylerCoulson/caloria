3cfbbf9aba4000c3c380e5adc7791185b1d7c116
Tyler Coulson <tecoulson92@gmail.com>
2023-05-01 15:18:50 -0400

    add docker files

--- app/config.py
+++ app/config.py
@@ -1,17 +1,16 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 import aiosqlite
 
 class Settings(BaseSettings):
     API_KEY: str = ""
     API_V1_STR: str = ""
-    SQLALCHEMY_DATABASE_URI: str = ""
+    SQLALCHEMY_DATABASE_URI: str = Field(..., env='DATABASE_URL')
     SQLALCHEMY_TEST_DATABASE_URI: str = ""
     DATE_FORMAT: str = "%Y-%m-%dT%H:%M:%S.%f"
     APP_NAME: str = ""
 
 
 settings = Settings(
-    SQLALCHEMY_DATABASE_URI="sqlite+aiosqlite:///./app/sql_app.db",
     SQLALCHEMY_TEST_DATABASE_URI="sqlite+aiosqlite:///./app/tests/test.db",
     APP_NAME="EVERLIGHT"
 )
