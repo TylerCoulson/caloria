948a9e3459b4605e485f97f220a2318f04a99b4e
Tyler Coulson <tecoulson92@gmail.com>
2023-05-01 16:13:13 -0400

    delete fly database url

--- app/config.py
+++ app/config.py
@@ -11,7 +11,6 @@ class Settings(BaseSettings):
 
 
 settings = Settings(
-    SQLALCHEMY_DATABASE_URI="postgresql+asyncpg://postgres:dXo5tI2SovuAsso@track-food-db.flycast:5432",
     SQLALCHEMY_TEST_DATABASE_URI="sqlite+aiosqlite:///./app/tests/test.db",
     APP_NAME="EVERLIGHT"
 )
