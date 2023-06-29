4454ff6f434f8b74c2a828b0c2273915a7533b57
Tyler Coulson <tecoulson92@gmail.com>
2023-06-29 16:44:23 -0400

    added test for delete daily overview by id

--- app/tests/test_daily_output.py
+++ app/tests/test_daily_output.py
@@ -132,7 +132,7 @@ async def test_daily_overview_update(client:TestClient, db:Session):
 
     assert content.keys() == output.keys() 
 
-async def test_daily_overview_delete(client:TestClient, db:Session):    
+async def test_daily_overview_delete_by_date(client:TestClient, db:Session):    
     end_date = "2023-12-07"
 
     daily = {
@@ -144,6 +144,24 @@ async def test_daily_overview_delete(client:TestClient, db:Session):
 
 
     response= await client.delete(f"/api/v1/daily/{data.date}")
+    assert response.status_code == 200
+
+    assert response.json() is None
+
+    assert await crud.read(_id=data.id, db=db, model=models.DailyLog)  is None
+
+async def test_daily_overview_delete_by_id(client:TestClient, db:Session):    
+    end_date = "2023-12-07"
+
+    daily = {
+        "profile_id": 1,
+        "date": end_date,
+        "actual_weight": 308.8
+    }
+    data = await crud.create(obj_in=schemas.DailyOverviewInput(**daily), db=db, model=models.DailyLog)
+
+
+    response= await client.delete(f"/api/v1/daily/{data.id}")
     assert response.status_code == 200
 
     assert response.json() is None
