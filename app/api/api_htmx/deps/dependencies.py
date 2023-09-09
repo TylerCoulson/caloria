c109fbf2abb9b7e1a58f9118c6e6e2eda82464f6
Tyler Coulson <tecoulson92@gmail.com>
2023-09-08 20:11:53 -0400

    Changed htmx deps from annotated user to annotated profile

--- app/api/api_htmx/deps/dependencies.py
+++ app/api/api_htmx/deps/dependencies.py
@@ -2,18 +2,18 @@ from fastapi import Depends, Request, Header
 from sqlalchemy.orm import Session
 from typing import Annotated
 
-from app.auth.router import Annotated_User
+from app.auth.router import Annotated_Profile
 from app import deps
 
 
 
-async def common_deps(request: Request, profile: Annotated_User = False, hx_request: str | None = Header(default=None), db: Session = Depends(deps.get_db)):
+async def common_deps(request: Request, profile: Annotated_Profile = False, hx_request: str | None = Header(default=None), db: Session = Depends(deps.get_db)):
     return {"request":request, "profile":profile, "hx_request":hx_request, "db":db}
 
 CommonDeps = Annotated[dict, Depends(common_deps)]
 
 
-async def logged_in_deps(common:CommonDeps, profile: Annotated_User):
+async def logged_in_deps(common:CommonDeps, profile: Annotated_Profile):
     return {**common, "profile":profile}
 
 LoggedInDeps = Annotated[dict, Depends(logged_in_deps)]
