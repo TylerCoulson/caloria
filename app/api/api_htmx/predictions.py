ae663a44957000691133d500e0625c67a4879029
Tyler Coulson <tecoulson92@gmail.com>
2023-09-09 14:26:55 -0400

    updated predictions to prefill inputs if loggedin

--- app/api/api_htmx/predictions.py
+++ app/api/api_htmx/predictions.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends, status,Request, Header
+from fastapi import APIRouter, Depends, status
 from fastapi.responses import HTMLResponse
-from sqlalchemy.orm import Session  # type: ignore
 from fastapi.templating import Jinja2Templates
 
 from app.api.api_V1 import predictions
 from app.api.api_V1.predictions import weight_params
 from app.api.api_htmx.deps import CommonDeps
+from app.auth.router import Annotated_Profile
 
 router = APIRouter(prefix="/prediction")
 templates = Jinja2Templates("app/templates")
@@ -16,13 +16,14 @@ templates = Jinja2Templates("app/templates")
     response_class=HTMLResponse,
     status_code=status.HTTP_200_OK,
 )
-def get_predictions_page(*, deps:CommonDeps):
+async def get_predictions_page(*, deps:CommonDeps, profile:Annotated_Profile=False):
     """ returns page that allows foods searching for food"""
 
     context = {
             "request": deps['request'],
             "hx_request": deps['hx_request'],
             "user": deps['user'],
+            "profile": profile if profile else {"start_date":"", "start_weight":"", "goal_weight":"", "sex":"", "birthdate":"", "height":"", "lbs_per_week":"", "activity_level":""}
         }
     return templates.TemplateResponse("prediction/base.html", context)
 
@@ -32,13 +33,14 @@ def get_predictions_page(*, deps:CommonDeps):
     response_class=HTMLResponse,
     status_code=status.HTTP_200_OK,
 )
-async def get_predictions_never_faulter(*, params: dict=Depends(weight_params), deps:CommonDeps):
+async def get_predictions_never_faulter(*, params: dict=Depends(weight_params), deps:CommonDeps, profile:Annotated_Profile=False):
     """ returns page for predictions that never faultered on calories lost """
-    pred = await predictions.get_predictions_never_fault(params=params, db=deps['db'])
+    pred = await predictions.get_predictions_never_fault(params=params)
     context = {
             "request": deps['request'],
             "hx_request": deps['hx_request'],
             "user": deps['user'],
+            "profile": profile if profile else {"start_date":"", "start_weight":"", "goal_weight":"", "sex":"", "birthdate":"", "height":"", "lbs_per_week":"", "activity_level":""},
             "preds": pred,
             "final_day": pred[str(len(pred)-1)]
         }
