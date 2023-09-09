344c635bae01d478dbbb565eb17d11e040f0b5d0
Tyler Coulson <tecoulson92@gmail.com>
2023-09-09 12:32:20 -0400

    added ability to add servings to food

--- app/api/api_htmx/serving_size.py
+++ app/api/api_htmx/serving_size.py
@@ -2,12 +2,11 @@ from fastapi import APIRouter, Depends, status,Request, HTTPException, Header
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from app import deps
+from app import schemas
 from app.api.api_htmx.deps import CommonDeps
 from app.api.api_V1 import serving_size as api_servings
 from app.api.api_htmx.food import router as food_router
 from app.api.api_V1 import food as api_food
-from app.auth.router import Annotated_User
 
 router = APIRouter()
 templates = Jinja2Templates("app/templates")
@@ -29,69 +28,25 @@ async def get_create_serving(*, deps:CommonDeps, food_id:int):
         }
     return templates.TemplateResponse("food/servings/create.html", context)
 
-# @router.post(
-#     "/servings",
-#     response_class=HTMLResponse,
-#     status_code=status.HTTP_201_CREATED,
-# )
-# async def post_servings(*, request: Request, hx_request: str | None = Header(default=None), serving_size: schemas.ServingSizeCreate, db: Session = Depends(deps.get_db)):
-#     servings_out = await api_servings.post_serving_size(serving_size=serving_size, db=db)
+@router.post(
+    "/servings",
+    response_class=HTMLResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+async def post_servings(*, deps:CommonDeps, serving_size: schemas.ServingSizeCreate):
+    servings_out = await api_servings.post_serving_size(deps=deps, serving_size=serving_size)
+    food = await api_food.get_food_id(deps=deps, food_id=serving_size.food_id)
+    context = {
+            "request": deps['request'],
+            "hx_request": deps['hx_request'],
+            "trigger": "click",
+            "user": deps['user'],
+            "food": food,
+            "servings": [servings_out]
+        }
 
-#     context = {
-#             "request": request,
-#             "hx_request": hx_request,
-#             "trigger": "click",
-#             "servings": [servings_out]
-#         }
+    return templates.TemplateResponse("food/item.html", context)
 
-#     return templates.TemplateResponse("servings.html", context)
-
-# @router.get(
-#     "/{food_id:int}/serving/{serving_id:int}",
-#     response_class=HTMLResponse,
-#     status_code=status.HTTP_200_OK,
-# )
-# async def get_serving_size_id(*, request: Request, hx_request: str | None = Header(default=None), serving_id: int, db: Session = Depends(deps.get_db)) -> list[schemas.FoodLog]:
-
-#     servings_out = await api_servings.get_serving_size_id(serving_id=serving_id, db=db)
-#     context = {
-#             "request": request,
-#             "hx_request": hx_request,
-#             "trigger": None,
-#             "servings": [servings_out]
-#         }
-#     return templates.TemplateResponse("servings.html", context)
-
-# @router.get(
-#     "/{food_id:int}/servings",
-#     response_class=HTMLResponse,
-#     status_code=status.HTTP_200_OK,
-# )
-# async def get_all_servings_for_a_food(*, request: Request, hx_request: str | None = Header(default=None), food_id: int, db: Session = Depends(deps.get_db)) -> list[schemas.FoodLog]:
-#     servings_out = await api_servings.get_serving_size_by_food(food_id=food_id, db=db)
-#     context = {
-#             "request": request,
-#             "hx_request": hx_request,
-#             "trigger": "click",
-#             "servings": servings_out['servings']
-#         }
-#     return templates.TemplateResponse("servings.html", context)
-
-
-# @router.get(
-#     "/servings",
-#     response_class=HTMLResponse,
-#     status_code=status.HTTP_200_OK,
-# )
-# async def get_all_servings_for_a_food(*, request: Request, hx_request: str | None = Header(default=None), food_id: int, db: Session = Depends(deps.get_db)) -> list[schemas.FoodLog]:
-#     servings_out = await api_servings.get_serving_size_by_food(food_id=food_id, db=db)
-#     context = {
-#             "request": request,
-#             "hx_request": hx_request,
-#             "trigger": "click",
-#             "servings": servings_out['servings']
-#         }
-#     return templates.TemplateResponse("food_log/serving_option.html", context)
 
 
 food_router.include_router(router, tags=['htmx-servings'])
