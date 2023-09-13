31d2d44a5724afcdd1cb28c78e8d43827c14d8cc
Tyler Coulson <tecoulson92@gmail.com>
2023-09-12 21:46:27 -0400

    Added check of serving size to check if food belongs to user

--- app/api/api_V1/serving_size.py
+++ app/api/api_V1/serving_size.py
@@ -4,9 +4,19 @@ from app import schemas
 from app import models
 from app import crud
 from app.api.api_V1.deps import CommonDeps
-from app.api.api_V1.food import router
+from app.api.api_V1.food import router, get_food_id, get_user_id
 
 router.tags = ['servings']
+
+async def check_food_authorized(deps:CommonDeps, food_id:int):
+    food = await get_food_id(deps=deps, food_id=food_id)
+
+    if food.user_id is None:
+        return
+    if food.user_id == get_user_id(deps=deps):
+        return
+
+    raise HTTPException(status_code=403, detail="Not Authorized")
 
 @router.post(
     "/{food_id}/serving",
@@ -14,6 +24,8 @@ router.tags = ['servings']
     status_code=status.HTTP_201_CREATED,
 )
 async def post_serving_size(*, deps:CommonDeps, serving_size: schemas.ServingSizeCreate):
+    await check_food_authorized(deps=deps, food_id=serving_size.food_id)
+    
     serving_size_out = await crud.create(obj_in=serving_size, db=deps['db'], model=models.ServingSize)
     return serving_size_out
 
@@ -22,7 +34,9 @@ async def post_serving_size(*, deps:CommonDeps, serving_size: schemas.ServingSiz
     response_model=schemas.ServingSize,
     status_code=status.HTTP_200_OK,
 )
-async def get_serving_size_id(*, deps:CommonDeps, food_id: int = None, serving_id: int):
+async def get_serving_size_id(*, deps:CommonDeps, food_id: int, serving_id: int):
+    await check_food_authorized(deps=deps, food_id=food_id)
+
     data = await crud.read(_id=serving_id, db=deps['db'], model=models.ServingSize)
     if not data:
         raise HTTPException(status_code=404, detail="Serving size not found")
@@ -34,6 +48,8 @@ async def get_serving_size_id(*, deps:CommonDeps, food_id: int = None, serving_i
     status_code=status.HTTP_200_OK,
 )
 async def get_serving_size_by_food(*, deps:CommonDeps, food_id: int):
+    await check_food_authorized(deps=deps, food_id=food_id)
+
     statement = select(models.ServingSize).where(models.ServingSize.food_id == food_id)
 
     data = await deps['db'].execute(statement)
@@ -45,7 +61,8 @@ async def get_serving_size_by_food(*, deps:CommonDeps, food_id: int):
     response_model=schemas.ServingSize,
     status_code=status.HTTP_200_OK,
 )
-async def update_serving_size(*, deps:CommonDeps, serving_id: int, serving_size_in: schemas.ServingSizeBase):
+async def update_serving_size(*, deps:CommonDeps, food_id:int, serving_id: int, serving_size_in: schemas.ServingSizeBase):
+    await check_food_authorized(deps=deps, food_id=food_id)
 
     data = await crud.update(_id=serving_id, model=models.ServingSize, update_data=serving_size_in, db=deps['db'])
     if not data:
@@ -58,6 +75,8 @@ async def update_serving_size(*, deps:CommonDeps, serving_id: int, serving_size_
     status_code=status.HTTP_200_OK,
 )
 async def delete_serving_size(*, deps:CommonDeps, food_id:int, serving_id:int):
+    await check_food_authorized(deps=deps, food_id=food_id)
+
     data = await get_serving_size_id(deps=deps, food_id=food_id, serving_id=serving_id)
     data = await crud.delete(_id=serving_id, db=deps['db'], db_obj=data)
     
--- app/tests/test_servings/test_servings_get_servings.py
+++ app/tests/test_servings/test_servings_get_servings.py
@@ -8,22 +8,22 @@ async def test_get_serving_size_by_food_exists(client, get_multiple_serving):
 async def test_get_serving_size_by_food_not_exists(client):
     # Test when food_id does not exist in the database
     response = await client.get("/api/v1/food/9999/servings")
-    assert response.status_code == 200
-    assert response.json() == {"servings":[]}
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Food not found"
 
 
 async def test_get_serving_size_by_food_negative(client):
     # Test when food_id is a negative value
     response = await client.get("/api/v1/food/-1/servings")
-    assert response.status_code == 200
-    assert response.json() == {"servings":[]}
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Food not found"
 
 
 async def test_get_serving_size_by_food_zero(client):
     # Test when food_id is zero
     response = await client.get("/api/v1/food/0/servings")
-    assert response.status_code == 200
-    assert response.json() == {"servings":[]}
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Food not found"
 
 
 async def test_get_serving_size_by_food_string(client):
