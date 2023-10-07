69a08e7a5aadd042013e9888679c36ca778ae5e8
Tyler Coulson <tecoulson92@gmail.com>
2023-10-07 14:17:40 -0400

    fixed issue with get_serving_size_id needing redundant food it

--- app/api/api_V1/serving_size.py
+++ app/api/api_V1/serving_size.py
@@ -36,12 +36,15 @@ async def post_serving_size(*, deps:CommonDeps, food_id:int, serving_size: schem
     response_model=schemas.ServingSize,
     status_code=status.HTTP_200_OK,
 )
-async def get_serving_size_id(*, deps:CommonDeps, food_id: int, serving_id: int):
-    await check_food_authorized(deps=deps, food_id=food_id)
+async def get_serving_size_id(*, deps:CommonDeps, food_id: int=None, serving_id: int):
 
     data = await crud.read(_id=serving_id, db=deps['db'], model=models.ServingSize)
+
     if not data:
         raise HTTPException(status_code=404, detail="Serving size not found")
+
+    await check_food_authorized(deps=deps, food_id=data.food_id)
+
     return data
 
 @router.get(
