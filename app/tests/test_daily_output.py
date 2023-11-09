56bde633a83ebbcfa0ab4956a5c1f15f0e733258
Tyler Coulson <tecoulson92@gmail.com>
2023-11-08 22:07:12 -0500

    added tests for daily_activity level

--- app/tests/test_daily_output.py
+++ app/tests/test_daily_output.py
@@ -53,7 +53,8 @@ async def test_daily_overview_get_on_start_date(client:TestClient, db:Session):
         "total_lbs_lost": 10.7,
         "calorie_surplus": -19540,
         "profile_id": food_log['profile_id'],
-        "bmi": 44.71
+        "bmi": 44.71,
+        "activity_level": 1.2
     }
 
     assert content.keys() == output.keys()
@@ -83,19 +84,20 @@ async def test_get_daily_after_start_date(client:TestClient, db:Session):
         "total_lbs_lost": 10.7,
         "calorie_surplus": -19540,
         "profile_id": food_log['profile_id'],
-        "bmi": 44.71
+        "bmi": 44.71,
+        "activity_level": 1.2
     }
     assert content.keys() == output.keys() 
 
 
-async def test_daily_overview_post(client:TestClient, db:Session):
+async def test_daily_overview_post_acutal_weight(client:TestClient, db:Session):
     food_log = { "id":12, "date":'2023-04-09', "food_id":123, "serving_size_id":123, "serving_amount":3.0, "profile_id":1}
     start_date = datetime.strptime(food_log['date'],'%Y-%m-%d')
 
     data = {
         "profile_id": 1,
         "date": (start_date).strftime('%Y-%m-%d'),
-        "actual_weight": 308.8
+        "actual_weight": 308.8,
     }
     response = await client.post(f"/api/v1/daily", json=data)
     content = response.json()    
@@ -103,7 +105,22 @@ async def test_daily_overview_post(client:TestClient, db:Session):
 
     assert content['actual_weight'] == 308.8
 
-async def test_daily_overview_update(client:TestClient, db:Session):    
+async def test_daily_overview_post_activity_level(client:TestClient, db:Session):
+    food_log = { "id":12, "date":'2023-04-09', "food_id":123, "serving_size_id":123, "serving_amount":3.0, "profile_id":1}
+    start_date = datetime.strptime(food_log['date'],'%Y-%m-%d')
+
+    data = {
+        "profile_id": 1,
+        "date": (start_date).strftime('%Y-%m-%d'),
+        "activity_level": 1.8,
+    }
+    response = await client.post(f"/api/v1/daily", json=data)
+    content = response.json()    
+    assert response.status_code == 201
+
+    assert content['activity_level'] == 1.8
+
+async def test_daily_overview_update_actual_weight(client:TestClient, db:Session):    
     food_log = { "id":12, "date":'2023-04-10', "food_id":123, "serving_size_id":123, "serving_amount":3.0, "profile_id":1}
     start_date = datetime.strptime(food_log['date'],'%Y-%m-%d')
     daily = {
@@ -136,10 +153,50 @@ async def test_daily_overview_update(client:TestClient, db:Session):
         "total_lbs_lost": 10.7,
         "calorie_surplus": -19540,
         "profile_id": 1,
-        "bmi":44.71
+        "bmi":44.71,
+        "activity_level": 1.2
     }
 
     assert content.keys() == output.keys() 
+
+async def test_daily_overview_update_activity_level(client:TestClient, db:Session):    
+    food_log = { "id":12, "date":'2023-04-10', "food_id":123, "serving_size_id":123, "serving_amount":3.0, "profile_id":1}
+    start_date = datetime.strptime(food_log['date'],'%Y-%m-%d')
+    daily = {
+        "profile_id": 1,
+        "date": food_log['date'],
+        "activity_level": 1.2
+    }
+    
+    data = await crud.create(obj_in=schemas.DailyOverviewInput(**daily), db=db, model=models.DailyLog)
+
+
+    data.activity_level == 1.8
+    response= await client.put(f"/api/v1/daily/{data.date}", json=jsonable_encoder(data))
+    assert response.status_code == 200
+    content = response.json()
+
+    output = {
+        "day": 0,
+        "actual_weight": 256.7,
+        "week": 0,
+        "date": 0,
+        "est_weight": 311.7,
+        "resting_rate": 2860,
+        "eaten_calories": 1900,
+        "eaten_fats": 1900,
+        "eaten_carbs": 1900,
+        "eaten_protein": 1900,
+        "calories_left": -40,
+        "calorie_goal": 1860,
+        "total_lbs_lost": 10.7,
+        "calorie_surplus": -19540,
+        "profile_id": 1,
+        "bmi":44.71,
+        "activity_level": 1.8
+    }
+
+    assert content.keys() == output.keys()
 
 async def test_daily_overview_delete_by_date(client:TestClient, db:Session):    
     end_date = "2023-12-07"
