27521f5f85fc9b4fd92b2a0a308cf46d8fe18a25
Tyler Coulson <tecoulson92@gmail.com>
2023-11-21 09:46:37 -0500

    create test for all post paths

--- app/tests/test_htmx/test_all.py
+++ app/tests/test_htmx/test_all.py
@@ -49,3 +49,23 @@ async def test_gets(client:TestClient, db:Session, url):
 
     assert response.status_code == 200
     assert response.headers["content-type"] == "text/html; charset=utf-8"
+
+
+@pytest.mark.parametrize(
+        "url",
+        methods['POST']
+)
+async def test_post(client:TestClient, db:Session, url):
+    data = {
+        # food
+        "id": 1001, "category_id": 2, "type": 'odio porttitor', "subtype": 'nec', 'category': {'description': 'Fast Food', 'id': 2}, "user_id": 1001,
+        # servings
+        "id": 1001, "food_id": 1, "description": 'nisi volutpat', "calories": 6121, "fats": 302, "carbs": 750, "protein": 372, "food": {"id": 1, "category_id": 2, "type": 'posuere', "subtype": 'viverra dapibus', "user_id": 1001},
+        # food log
+        "id":1001, "date":'2022-12-09', "food_id":123, "serving_size_id":123, "serving_amount":3, "profile_id":1
+    }
+    # params = {"food_category": 1, "search_word":"test", "food_id":1, "log_id":1, **weight_params, "email":"test123@test.com","password":"1231412321", "password_confirm":"1231412321"}
+    response = await client.post(url, json=data)
+
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
