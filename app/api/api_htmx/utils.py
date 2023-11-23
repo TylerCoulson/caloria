15a4739ea1ad99948e42ef4b9798144f54a924f3
Tyler Coulson <tecoulson92@gmail.com>
2023-11-22 21:54:57 -0500

    move calorie progress circumference calc to jinja

--- app/api/api_htmx/utils.py
+++ app/api/api_htmx/utils.py
@@ -1,26 +1,31 @@
 from app.api.api_V1.deps import CommonDeps, LoggedInDeps
 from app.api.api_V1 import daily_overview as api_daily
 
-async def calorie_progress_data(deps:LoggedInDeps, overview=None):
+async def calorie_progress_data(deps:LoggedInDeps, overview=None, total:bool=False):
+    """
+    overview - Data for the current day 
+    total - Use totals amounts from all days
+    """
     if overview is None:
         overview = await api_daily.get_daily(deps=deps, current_date=deps['client_date'])
     
-    circumference = 326.7256
+    if total:
+        pass
+
     offsets = {
             "calorie_info": {
                 "calories_burned":overview['calories_burned'],
                 "calories_eaten":overview['eaten_calories'],
                 "calorie_goal":overview['calorie_goal'],
             },
-            "circumference": circumference,
-            "white_offset_max": ((1 - (overview['calorie_goal']/overview['calories_burned'])) * circumference),
+            "white_offset_max": ((1 - (overview['calorie_goal']/overview['calories_burned']))),
             "white_offset": 
             max(
-                ((1 - min(overview['eaten_calories']/overview['calories_burned'], 1)) * circumference),
-                ((1 - (overview['calorie_goal']/overview['calories_burned'])) * circumference)
+                ((1 - min(overview['eaten_calories']/overview['calories_burned'], 1))),
+                ((1 - (overview['calorie_goal']/overview['calories_burned'])))
             ),
-            "yellow_offset":  ((1 - min(overview['eaten_calories']/overview['calories_burned'], 1)) * circumference),
-            "red_offset": min(
+            "yellow_offset":  ((1 - min(overview['eaten_calories']/overview['calories_burned'], 1))),
+            "red_offset":
                 (
                     (
                         1 - (
@@ -29,8 +34,7 @@ async def calorie_progress_data(deps:LoggedInDeps, overview=None):
                                 1
                             )
                         )
-                    ) * circumference
+                    )
                 ), 
-                circumference),
         }
     return offsets
